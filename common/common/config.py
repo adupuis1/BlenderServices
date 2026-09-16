c91c550b88f86c9e7b2592ddf00b8282e30e0a4c
Anatole Dupuis <anatole.dupuis@gmail.com>
2026-09-16 13:43:30 -0400

    env set up and initialization

---
+++ common/common/config.py
@@ -0,0 +1,11 @@
+from pyantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_ignore_empty=True,
+        extra="ignore",
+    )
+    PROJECT_NAME: str = "Blender Services"
+    API_V1_STR: str = "/api/v1"
+    DATABASE_URL: str = "postgresql+psycopg://blender:dev@localhost:5433/blender"
